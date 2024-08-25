8b12798c280e35743dd4cd8343ba8eb7c1adba05
JBLACK <eduarsantor@gmail.com>
2024-08-25 10:52:25 -0600

    practice

--- contenedor/App1/admin.py
+++ contenedor/App1/admin.py
@@ -1,23 +1,11 @@
 from django.contrib import admin
 from.models import EmpresaDB, OfertaDB,ResenasDB, Habilidades, UsuarioPersonalizado
 
-
 # Register your models here.
-#admin.site.register(UsuarioDB)
+
 admin.site.register(UsuarioPersonalizado)
 admin.site.register(EmpresaDB)     
 admin.site.register(Habilidades)
 admin.site.register(OfertaDB)
 admin.site.register(ResenasDB)
-
-
-#class OfertaAdmin(admin.ModelAdmin):
-   # list_display = ('titulo', 'empleador', 'topicos')
-   # search_fields = ('titulo', 'descripcion')
-   # list_filter = ('topicos')
-
-
-
-
-
 
